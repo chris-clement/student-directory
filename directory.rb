0b03f0a258a2fae1b29ad0e52994e166d8a21492
chris-clement <chris.clement1995@gmail.com>
2021-11-08 16:28:48 +0000

    Cleaned up some repitive code and made students an instance variable to be accessed across methods

--- directory.rb
+++ directory.rb
@@ -1,19 +1,49 @@
+@students = []
+
 def input_students
   puts "Please enter the name of the students"
   puts "To finish, just hit the return key twice."
-  #create an empty array
-  students = []
-  # while the name is not empty, repeat the below code.
+  # get the first name
   name = gets.chomp
   while !name.empty? do
-    #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    #get another name from the user
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
     name = gets.chomp
   end
-  # return array of students
-  students
+end
+
+def interactive_menu
+  loop do 
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" 
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection 
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I'm not sure what you mean, try again"
+  end
 end
 
 def print_header
@@ -21,38 +51,16 @@ def print_header
   puts "-------------"
 end
     
-def print(students)
-  students.each.with_index(1) do |student, index|
+def print_student_list
+  @students.each.with_index(1) do |student, index|
     puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-def interactive_menu
-  students = []
-  loop do 
-  # 1. print the menu and ask the user what to do 
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection 
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I'm not sure what you mean, try again"
-    end
-  end
-end
 interactive_menu
 
 
